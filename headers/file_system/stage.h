ea1656e504b0e03c186fdb63a5e67787ba87f5b9
Increedible <108118527+Increedible@users.noreply.github.com>
2025-07-31 18:01:50 -0400

    Fixed Karel's wrong merge conflict thing

--- headers/file_system/stage.h
+++ headers/file_system/stage.h
@@ -1,17 +1,20 @@
 #pragma once
 #include <string>
+#include <array>
+#include <vector>
 #include <optional>
 #include "../misc/utilities.h"
+#include "../misc/io_utils.h"
 
 struct Stage {
     int id;
-    std::string name;
-    std::string dialogue, conclusion;
-    int directions[4];
+    StyleString name;
+    StyleString dialogue, conclusion;
+    std::array<int,4> directions;
     image<16> vis;
     image<6> legend;
     int needcoins, coinreward;
     std::optional<int> shop, minigame;
-    std::optional<std::pair<int, std::string>> encounter; // opponent, dialogue
+    std::optional<std::pair<int, StyleString>> encounter; // opponent, dialogue
     std::vector<std::pair<int, double>> possibleEncounters; // opponent, probability
 };
